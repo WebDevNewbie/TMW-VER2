b2b4b59d6746e1aabf667c391c2e798c687bef52
Stephen Cabalida <Stephen.Cabalida@sizmek.com>
2018-09-12 23:41:37 +0800

    profile updated

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -409,10 +409,17 @@ angular.module('tradeapp.controllers', [])
 		}							  
     }
 })
-.controller('UserProfileCtrl', function($scope,  $rootScope,  $ionicLoading,  $ionicPlatform,  Auth, $ionicHistory) {
+.controller('UserProfileCtrl',['$scope','$window','$ionicActionSheet','$ionicModal','$rootScope','$ionicPlatform','$ionicHistory','$ionicLoading','$ionicPopup','Auth',
+                     function($scope, $window, $ionicActionSheet, $ionicModal, $rootScope,  $ionicPlatform,  $ionicHistory,  $ionicLoading, $ionicPopup, Auth){
 	
 	$scope.user = [];
+	$scope.user.fname_error = false;
+	$scope.user.lname_error = false;
+	$scope.user.age_error = false;
+	$scope.user.bday_error = false;
+	$scope.user.address_error = false;
 	$scope.user.clickedEdit = false;
+	
 	$scope.editProfile = function()
     {
 		$scope.user.clickedEdit = true;
@@ -420,14 +427,186 @@ angular.module('tradeapp.controllers', [])
 	}
 	$scope.saveProfile = function()
     {
-		$scope.user.clickedEdit = false;
-		$("#profile-wrapper label input").prop("disabled", true);
+		//$scope.user.clickedEdit = false;
+		//$("#profile-wrapper label input").prop("disabled", true);
+		if($scope.user.fname == undefined) $scope.user.fname="";  
+		if($scope.user.lname == undefined) $scope.user.lname="";  
+		if($scope.user.age == undefined) $scope.user.age="";  
+		if($scope.user.address == undefined) $scope.user.address="";  
+		if($scope.user.activity1 == undefined) $scope.user.activity1="";  
+		if($scope.user.activity2 == undefined) $scope.user.activity2="";  
+		if($scope.user.activity3 == undefined) $scope.user.activity3="";  
+		if($scope.user.occupation == undefined) $scope.user.occupation=""; 
+		if($scope.user.hobbies == undefined) $scope.user.hobbies=""; 
+		if($scope.user.skill == undefined) $scope.user.skill=""; 
+		if($scope.user.learn == undefined) $scope.user.learn=""; 
+		if($scope.user.todo == undefined) $scope.user.todo=""; 
+		if($scope.user.visit == undefined) $scope.user.visit=""; 
+		if($scope.user.language == undefined) $scope.user.language=""; 
+		if($scope.user.education == undefined) $scope.user.education=""; 
+		if($scope.user.collegecourse == undefined) $scope.user.collegecourse=""; 
+		if($scope.user.certificate == undefined) $scope.user.certificate=""; 
+		if($scope.user.religion == undefined) $scope.user.religion=""; 
+		if($scope.user.children == undefined) $scope.user.children=""; 
+		if($scope.user.ethniticity == undefined) $scope.user.ethniticity=""; 
+		
+		var validInput = $scope.VALIDATE_EDITPROFILE_INPUT();
+		if(validInput){
+			var tDate = new Date(document.getElementById("user_bday").value);
+			var monD = tDate.getMonth() + 1;
+			var todD = tDate.getDate();
+			if(monD < 10){ monD = "0"+monD }
+			if(todD < 10){ todD = "0"+todD }
+			var newDate = tDate.getFullYear()+"-"+monD+"-"+todD;
+			$scope.user.bday = newDate;
+			console.log($scope.user.bday);
+			$scope.UPDATE_TO_SERVER();
+			console.log("add to server");
+
+		}else{
+			console.log("invalid input");
+		}
 	}
+	$scope.VALIDATE_EDITPROFILE_INPUT = function ()
+	  {
+		var ret = true;
+		$scope.clean_fname = $rootScope._remove_white_space($scope.user.fname);
+		$scope.clean_lname = $rootScope._remove_white_space($scope.user.lname);
+		$scope.clean_age = $rootScope._remove_white_space($scope.user.age);
+		
+		$scope.clean_address = $rootScope._remove_white_space($scope.user.address);
+
+		var return_bol = true;
+		
+		
+		if(!$rootScope.inputBlank($scope.clean_fname))
+		{
+		  $scope.user.fname_error = true;
+		  $scope.user.fname_desc = "Please fill out this field.";
+		  ret = false;
+		} else {
+		  $scope.user.fname_error = false;
+		}
+		if(!$rootScope.inputBlank($scope.clean_lname))
+		{
+		  $scope.user.lname_error = true;
+		  $scope.user.lname_desc = "Please fill out this field.";
+		  ret = false;
+		} else {
+		  $scope.user.lname_error = false;
+		}
+		if(!$rootScope.inputBlank($scope.clean_age))
+		{
+		  $scope.user.age_error = true;
+		  $scope.user.age_desc = "Please fill out this field.";
+		  ret = false;
+		} else {
+			if(!$rootScope.isNumber($scope.clean_age)){
+				$scope.user.age_error = true;
+				$scope.user.age_desc = "Please input a number.";
+			}else{
+				$scope.user.age_error = false;
+			}
+		}
+		if(!$rootScope.inputBlank(document.getElementById("user_bday").value))
+		{
+		  $scope.user.bday_error = true;
+		  $scope.user.bday_desc = "Please fill out this field.";
+		  ret = false;
+		} else {
+		  $scope.user.bday_error = false;
+		}
+		if(!$rootScope.inputBlank($scope.clean_address))
+		{
+		  $scope.user.address_error = true;
+		  $scope.user.address_desc = "Please fill out this field.";
+		  ret = false;
+		} else {
+		  $scope.user.address_error = false;
+		}
+
+		return ret;
+
+	  }
+	
+	$scope.UPDATE_TO_SERVER = function ()
+	{
+		var actArray = [];
+		$ionicLoading.show({
+          template: '<ion-spinner class="spinner-calm" icon="android"></ion-spinner>'
+        });
+		if($scope.user.activity1 == "" && $scope.user.activity2 == "" && $scope.user.activity3 =="")
+		{
+			actArray = [];
+		}else{
+			if($scope.user.activity1 != ""){actArray.push($scope.user.activity1);}
+			if($scope.user.activity2 != ""){actArray.push($scope.user.activity2);}
+			if($scope.user.activity3 != ""){actArray.push($scope.user.activity3);}
+		}	
+			
+        var obj    = new Object();
+        obj.method = 'POST';
+        obj.url    = $rootScope.baseURL + "/mobile/user_controller/updateUser";
+        obj.data   = new FormData();
+        obj.data.append('user_id',$rootScope.user_info.user_id);
+        obj.data.append('fname',$scope.user.fname);
+        obj.data.append('lname',$scope.user.lname);
+        obj.data.append('age',$scope.user.age);
+        obj.data.append('address',$scope.user.address);
+        obj.data.append('bday',$scope.user.bday);
+        obj.data.append('activity',actArray);
+        obj.data.append('occupation',$scope.user.occupation);
+        obj.data.append('hobbies',$scope.user.hobbies);
+        obj.data.append('skill',$scope.user.skill);
+        obj.data.append('learn',$scope.user.learn);
+        obj.data.append('todo',$scope.user.todo);
+        obj.data.append('visit',$scope.user.visit);
+        obj.data.append('language',$scope.user.language);
+        obj.data.append('education',$scope.user.education);
+        obj.data.append('collegecourse',$scope.user.collegecourse);
+        obj.data.append('certificate',$scope.user.certificate);
+        obj.data.append('prefer_group',document.getElementById("user_group").value);
+        obj.data.append('prefer_place',document.getElementById("user_place_prefer").value);
+        obj.data.append('civil_status',document.getElementById("user_civil_status").value);
+        obj.data.append('live_athome',document.getElementById("user_live_athome").value);
+        obj.data.append('religion',$scope.user.religion);
+        obj.data.append('children',$scope.user.children);
+        obj.data.append('ethniticity',$scope.user.ethniticity);
+        obj.data.append('loginSecret','0ff9346b4edc8dc033bff30762bc3c15d465d3f');
+        obj.params = {};
+   
+			Auth.REQUEST(obj).then(
+                function(success) { 
+                    console.log(success.data);
+					$ionicLoading.hide();
+					if(success.data.success == true){
+						console.log("success"); 
+						$scope.user.clickedEdit = false;
+						$("#profile-wrapper label input").prop("disabled", true);
+						$scope.showEditSuccessMessage();
+					}
+					             
+                },
+                function(error) { 
+                    $ionicLoading.hide();
+               }
+            ); 
+	}
+	$scope.showEditSuccessMessage = function() {
+		   var alertPopup = $ionicPopup.alert({
+			 title: 'SUCCESS',
+			 template: 'Your profile is now updated.'
+		   });
+		};  
 	$scope.goBack = function()
     {
 		//$ionicHistory.goBack();
 		console.log(333);
 		//window.location.href = "#/menu/usersearch";
+	}
+	$scope.replaceAll = function(str, find, replace)
+    {
+		return str.replace(new RegExp(find, 'g'), replace);
 	}
 	$scope.$on('$ionicView.enter', function(event) {
 		//console.log("get data");
@@ -446,14 +625,38 @@ angular.module('tradeapp.controllers', [])
 					function(success) { 
 						if(JSON.stringify(success.data.success) == "true"){
 							
-							setTimeout(function(){ $ionicLoading.hide(); }, 1000);
+							var act = success.data.user_info.activity;
+							var actini = $scope.replaceAll(act,'"','');
+							var actfinal = actini.split(",");
+							setTimeout(function(){ $ionicLoading.hide(); }, 500);
 							console.log(success.data.user_info);
-							console.log(success.data.user_info.birthday);
+
+							console.log(actfinal);
 							document.getElementById("user_bday").value = success.data.user_info.birthday;
 							$scope.user.fname = success.data.user_info.first_name;
 							$scope.user.lname = success.data.user_info.last_name;
 							$scope.user.age = success.data.user_info.age;
 							$scope.user.address = success.data.user_info.address;
+							$scope.user.activity1 = actfinal[0];
+							$scope.user.activity2 = actfinal[1];
+							$scope.user.activity3 = actfinal[2];
+							$scope.user.occupation = success.data.user_info.occupation;
+							$scope.user.hobbies = success.data.user_info.hobbies;
+							$scope.user.skill = success.data.user_info.skills;
+							$scope.user.learn = success.data.user_info.learn;
+							$scope.user.todo = success.data.user_info.todo;
+							$scope.user.visit = success.data.user_info.visit;
+							$scope.user.language = success.data.user_info.languages;
+							$scope.user.education = success.data.user_info.education;
+							$scope.user.collegecourse = success.data.user_info.collegecourse;
+							$scope.user.certificate = success.data.user_info.certificate;
+							document.getElementById("user_group").value = success.data.user_info.prefer_group;
+							document.getElementById("user_place_prefer").value = success.data.user_info.prefer_place;
+							$scope.user.religion = success.data.user_info.religion;
+							document.getElementById("user_civil_status").value = success.data.user_info.civil_status;
+							document.getElementById("user_live_athome").value = success.data.user_info.live_athome;
+							$scope.user.children = success.data.user_info.children;
+							$scope.user.ethniticity = success.data.user_info.ethniticity;
 						}
 						else{
 							setTimeout(function(){ $ionicLoading.hide(); }, 1000);
@@ -469,7 +672,7 @@ angular.module('tradeapp.controllers', [])
 	})
 	
 	
-})
+}])
 .controller('ChatsCtrl', function($scope,  $rootScope,  $ionicLoading,  $ionicPlatform,  Auth) {
 
   $scope.chats = Chats.all();
