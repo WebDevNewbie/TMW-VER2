ccdab207a7242c020559c8cc420c91ea96a3249d
devnew <clickfunnelsdev@gmail.com>
2018-11-26 02:38:49 +0800

    baseurl changed

--- www/js/services.js
+++ www/js/services.js
@@ -5,9 +5,9 @@ app.factory('Auth', ['$http','$rootScope',
   // $rootScope.baseURL   = 'http://tradeapp.com';
   // prod backend connection url
   // $rootScope.baseURL   = 'http://localhost/tradeappbackend/public_html';
-  // $rootScope.baseURL   = 'http://trademyworld.club/tradeappbackend/public_html';
+  $rootScope.baseURL   = 'http://trademyworld.club/tradeappbackend/public_html';
   // local backend connection url
-  $rootScope.baseURL   = 'http://192.168.1.31/tradeappbackend/public_html';
+  // $rootScope.baseURL   = 'http://192.168.1.31/tradeappbackend/public_html';
   // set backend connection here
   // $rootScope.baseURL   = '';
 	
